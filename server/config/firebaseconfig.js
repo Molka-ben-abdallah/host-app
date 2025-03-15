1895f6b04e0ac1385fca5f574f7fbb79237af30d
asmabenhenia <asmabenhenia4@gmail.com>
2025-03-15 17:25:52 +0100

    Applied gitignore properly

--- server/config/firebaseconfig.js
+++ server/config/firebaseconfig.js
@@ -1,9 +1,16 @@
 const admin = require("firebase-admin");
 const dotenv = require("dotenv");
-dotenv.config(); 
 
+// Load environment variables from .env file
+dotenv.config();
+
+// Initialize Firebase Admin using the environment variables
 admin.initializeApp({
-  credential: admin.credential.cert("./config/serviceAccountKey.json"),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Fix the newlines in the private key
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 module.exports = admin;
